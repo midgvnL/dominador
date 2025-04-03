d42233045f3acc3c9c3e1510729e0574629424f7
midgvnL <midyle.lourencao@aluno.senai.br>
2025-04-03 07:38:00 -0300

    fdsvadia pt2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let titulo = document.getElementById("titulo");
 console.log(titulo[0]);
 
-//   chamnado por classe
+// //   chamnado por classe
 let esportes = document.getElementsByClassName (".esportes");
 console.log(esportes[0]);
 let esportes1 = document.getElementsByClassName (".esportes");
@@ -11,7 +11,7 @@ console.log(esportes2[0]);
 let esportes4 = document.getElementsByClassName (".esportes");
 console.log(esportes4[0]);
 
-//selecionando pelo nome da tag
+// //selecionando pelo nome da tag
 let LIespF = document.getElementsByTagName("li");
 console.log(LIespF[0]);
 let LIespB = document.getElementsByTagName("li");
@@ -24,8 +24,22 @@ console.log(LIespN[3]);
 //Selecionado pelo query selector
 // "#"= id "."= classe
 
-let tituloQS = document.querySelector("#titulo"+ tituloQS)
-let LIespFQs = document.querySelector(".esportes" + li[0].textContent)
-let LIespBQs = document.querySelector(".esportes" + li[1].textContent)
-let LIespVQs = document.querySelector(".esportes" + li[2].textContent)
-let LIespNQs = document.querySelector(".esportes" + li[3].textContent)
+let tituloQS = document.querySelector("#titulo");
+let LIespFQs = document.querySelector(".esportes");
+
+
+titulo.innerText = "Meus Esportes"
+titulo.innerHTML = "<span style='color:blue'> Meus Esportes </span>";
+
+let lista = document.getElementById("esportes");
+
+let Tenis = document.createElement("li");
+Tenis.innerText = "Tenis de mesa";
+
+
+let Patinacao = document.createElement("li");
+Patinacao.innerText = "Patinação no gelo";
+
+
+lista.appendChild(Tenis);
+lista.appendChild(Patinacao);
